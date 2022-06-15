bbff00e2d79ef517dc04bdf50ee080dab1a2c1f5
Fhao <1510719487@qq.com>
2022-06-15 16:33:43 +0800

    second commit

--- src/main/java/com/idheima/helloworld.java
+++ src/main/java/com/idheima/helloworld.java
@@ -3,6 +3,7 @@ package com.idheima;
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 
 }
